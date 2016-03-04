0cc47937dd88b19cec49ca06428d6609541fca8f
hsky <xezw211@126.com>
2016-03-05 00:08:37 +0800

    微信控制器

--- app/Http/Controllers/Front/IndexController.php
+++ app/Http/Controllers/Front/IndexController.php
@@ -22,18 +22,19 @@ class IndexController extends Controller
      * @return        
      */
     public function index(Request $request){
-        \Log::info(request());
+        $postData = file_get_contents("php://input");
+        // \Log::info(request());
         // \Log::info(request()->all());
 
-        $str = '<xml>
-<ToUserName><![CDATA[gh_88c164149eee]]></ToUserName>
-<FromUserName><![CDATA[oYCsEj6T9NqEpY1wvgddqhSVUWkk]]></FromUserName>
-<CreateTime>1457072778</CreateTime>
-<MsgType><![CDATA[text]]></MsgType>
-<Content><![CDATA[涓夊浗鏉€]]></Content>
-<MsgId>6258079929803722299</MsgId>
-<Encrypt><![CDATA[HA4avQ1ziXHFLPI2IScAh8GFB7ccFCrC1+GhbmpLp57OLL+SNmpNSDeFVNKLcfZtGn+gAx+pXo8aaCS93fVtkQi8B/307EEMuJoDKEkwFY3Z73nLvCpCNijElzQoGkk0ceCJHD5YkMklLSsy2vVwIBGLNW4tq8SE7LMOAOMko75Kl5U9uk/j4xBNRI7LRQQ3qcLgFNZvta7tzL+l+GM9WozTDsKoPLEN5GBU5fZcuNA2fOV8kmoNcGkJxgreEyKTe2XDiZQ3WZddsuz4RVIF2EVIMOE742t1S0O+1QDxbB9hbctQGe4l/PmNTWo7bZjUe39C48vj8ehemUYVhE1mo8AKTiNCleicFpBnzjK0uGQ/2fKY0kzFQox+6XNAiDAI8Abv7fRj0MF835UU/02jiBodMf7NjU596vEXxIZALbxG9OR8qFWovEZ5RcebNqcqAN56JkN1P/nb3bA+StTN9g==]]></Encrypt>
-</xml>';
+//         $str = '<xml>
+// <ToUserName><![CDATA[gh_88c164149eee]]></ToUserName>
+// <FromUserName><![CDATA[oYCsEj6T9NqEpY1wvgddqhSVUWkk]]></FromUserName>
+// <CreateTime>1457072778</CreateTime>
+// <MsgType><![CDATA[text]]></MsgType>
+// <Content><![CDATA[涓夊浗鏉€]]></Content>
+// <MsgId>6258079929803722299</MsgId>
+// <Encrypt><![CDATA[HA4avQ1ziXHFLPI2IScAh8GFB7ccFCrC1+GhbmpLp57OLL+SNmpNSDeFVNKLcfZtGn+gAx+pXo8aaCS93fVtkQi8B/307EEMuJoDKEkwFY3Z73nLvCpCNijElzQoGkk0ceCJHD5YkMklLSsy2vVwIBGLNW4tq8SE7LMOAOMko75Kl5U9uk/j4xBNRI7LRQQ3qcLgFNZvta7tzL+l+GM9WozTDsKoPLEN5GBU5fZcuNA2fOV8kmoNcGkJxgreEyKTe2XDiZQ3WZddsuz4RVIF2EVIMOE742t1S0O+1QDxbB9hbctQGe4l/PmNTWo7bZjUe39C48vj8ehemUYVhE1mo8AKTiNCleicFpBnzjK0uGQ/2fKY0kzFQox+6XNAiDAI8Abv7fRj0MF835UU/02jiBodMf7NjU596vEXxIZALbxG9OR8qFWovEZ5RcebNqcqAN56JkN1P/nb3bA+StTN9g==]]></Encrypt>
+// </xml>';
 
         // $xml = simplexml_load_string($str);
 
@@ -42,11 +43,12 @@ class IndexController extends Controller
         //     echo $child, '<br />';
         // }
 
-        $formatter = Formatter::make($str, Formatter::XML);
+        $formatter = Formatter::make($postData, Formatter::XML);
 
         $xmlArr = $formatter->toArray();
 
-        print $xmlArr['ToUserName'];
+        // print $xmlArr['ToUserName'];
+        \Log::info($xmlArr);
     }
 
 }
